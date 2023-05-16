806adc2a99b2e10b3939124d1ae92fdf05ca5c02
MAwaisme <73178485+MAwaisme@users.noreply.github.com>
2023-05-16 19:13:40 +0500

    home

--- src/components/home/GeneralTasks.js
+++ src/components/home/GeneralTasks.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Dropdown, Table } from "react-bootstrap";
 import dosts from "../../assets/icons/dots.svg";
+import submitIcon from "../../assets/icons/submitIcon.svg";
 
 const GeneralTasks = () => {
   return (
@@ -25,8 +26,75 @@ const GeneralTasks = () => {
             </td>
             <td>12:34 12/12/23</td>
             <td>
-              <div className="">
-                <img src={dosts} alt="dosts" />
+              <div className="tbl-dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <img src={dosts} alt="dosts" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="stats-dropdown-menu">
+                    <div className="stats-dropdown-bg">
+                      <Dropdown.Item>
+                        <img src={submitIcon} alt="submitIcon" />
+                        Submit Proof
+                      </Dropdown.Item>
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Like our facebook page</td>
+            <td>+5</td>
+            <td>
+              <div className="pending">Pending</div>
+            </td>
+            <td>
+              <div className="expired">Expired</div>
+            </td>
+            <td>
+              <div className="tbl-dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <img src={dosts} alt="dosts" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="stats-dropdown-menu">
+                    <div className="stats-dropdown-bg">
+                      <Dropdown.Item>
+                        <img src={submitIcon} alt="submitIcon" />
+                        Submit Proof
+                      </Dropdown.Item>
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Like our facebook page</td>
+            <td>+5</td>
+            <td>
+              <div className="pending">Pending</div>
+            </td>
+            <td>12:34 12/12/23</td>
+            <td>
+              <div className="tbl-dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <img src={dosts} alt="dosts" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="stats-dropdown-menu">
+                    <div className="stats-dropdown-bg">
+                      <Dropdown.Item>
+                        <img src={submitIcon} alt="submitIcon" />
+                        Submit Proof
+                      </Dropdown.Item>
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             </td>
           </tr>
@@ -38,8 +106,21 @@ const GeneralTasks = () => {
             </td>
             <td>12:34 12/12/23</td>
             <td>
-              <div className="">
-                <img src={dosts} alt="dosts" />
+              <div className="tbl-dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <img src={dosts} alt="dosts" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="stats-dropdown-menu">
+                    <div className="stats-dropdown-bg">
+                      <Dropdown.Item>
+                        <img src={submitIcon} alt="submitIcon" />
+                        Submit Proof
+                      </Dropdown.Item>
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             </td>
           </tr>
@@ -51,8 +132,21 @@ const GeneralTasks = () => {
             </td>
             <td>12:34 12/12/23</td>
             <td>
-              <div className="">
-                <img src={dosts} alt="dosts" />
+              <div className="tbl-dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <img src={dosts} alt="dosts" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="stats-dropdown-menu">
+                    <div className="stats-dropdown-bg">
+                      <Dropdown.Item>
+                        <img src={submitIcon} alt="submitIcon" />
+                        Submit Proof
+                      </Dropdown.Item>
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             </td>
           </tr>
