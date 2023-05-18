78238adfe04a46b120fb69f48617f69ca4e54056
hamzach625 <hamzach625@gmail.com>
2023-05-18 05:38:47 -0700

    hamza changes

--- src/screens/ClaimRewards.js
+++ src/screens/ClaimRewards.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import "./claim.scss"
+import AvailableReward from './AvailableReward';
+import RewardProcess from './RewardProcess';
+import ClaimedReward from './ClaimedReward';
+
+
+
 
 const ClaimRewards = () => {
   return (
-    <div>ClaimRewards</div>
+    <>
+      <section className="claim-section">
+        <Tabs
+          defaultActiveKey="availablereward"
+          id="uncontrolled-tab-example"
+          className="opeartions-tab"
+        >
+          <Tab eventKey="availablereward" title="Available Rewards">
+            <AvailableReward />
+          </Tab>
+          <Tab eventKey="rewardprocess" title="Rewards In Procress">
+            <RewardProcess />
+          </Tab>
+          <Tab eventKey="claimed" title="Claimed">
+            <ClaimedReward />
+          </Tab>
+        </Tabs>
+      </section>
+    </>
   )
 }
 
