6bb4bf099a04f861750b3559225605055ee08ff1
hamzach625 <hamzach625@gmail.com>
2023-07-18 16:19:51 +0500

    chages ui

---
+++ build/static/js/6.d84513f4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdao=this.webpackJsonpdao||[]).push([[6],{671:function(n,o){},691:function(n,o){},697:function(n,o){},699:function(n,o){}}]);
+//# sourceMappingURL=6.d84513f4.chunk.js.map
